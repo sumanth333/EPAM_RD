8c4627ee47adcbdec6516219f7876d7d00da22c4
sumanth333 <polesumanth333@gmail.com>
2018-06-21 23:27:08 +0530

    Added javadocs,naming conventions and Testcases code coverage

--- Maven/src/test/java/com/codebind/BubbleSortTest.java
+++ Maven/src/test/java/com/codebind/BubbleSortTest.java
@@ -1,15 +1,41 @@
 package com.codebind;
 
-import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class BubbleSortTest {
-
-	@Test
-	public void test() 
+//This is a JUnit framework for testing Bubble sort
+public class BubbleSortTest
+{
+	//This method is executed first
+	@BeforeClass
+	public static void BubbleSort()
 	{
-		
+		System.out.println("Before class is the first method executed");
+	}
+
+	//this method will be executed before each test
+	@Before
+	public void beforeEachTest()
+	{
+		System.out.println("Before method is executed before each test.");
+	}
+
+	//This is a test method you can create as many tests as you want
+	@Test
+	public void test()
+	{
+		System.out.println("This is a test method to test bubble sort");
+		BubbleSort.main(null);
+	}
+	//This is executed after each test
+	@After
+	public void afterEachTest()
+	{
+		System.out.println("This is exceuted after each Test");
+
 	}
 
 }
