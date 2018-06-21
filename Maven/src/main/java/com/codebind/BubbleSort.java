277eb6add00d817b3a29dbb1b82ffcf412831770
sumanth333 <polesumanth333@gmail.com>
2018-06-21 13:44:35 +0530

    Added javadocs and naming conventions

--- Maven/src/main/java/com/codebind/BubbleSort.java
+++ Maven/src/main/java/com/codebind/BubbleSort.java
@@ -4,27 +4,27 @@ package com.codebind;
 public class BubbleSort
 {
 	//prints the array
-	void printArray(int arr[], int length)
+	void printArray(int array[], int length)
 	{
 		for(int i=0; i<length; ++i)
-			System.out.print(arr[i]+" ");
+					System.out.print(array[i]+" ");
 		System.out.println();
 
 	}
 
 	//Bubble sort technique
-	void bubbleSort(int arr[], int length)
+	void bubbleSort(int array[], int length)
 	{
 		for(int i=0; i<length-1; ++i)
 		{
 			for(int j=0;j<length-i-1; ++j)
 			{
-				if(arr[j] > arr[j+1])
+				if(array[j] > array[j+1])
 				{
 					//swapping arr[j] and arr[j+1]
-					arr[j]   = arr[j] ^ arr[j+1];
-					arr[j+1] = arr[j] ^ arr[j+1];
-					arr[j]   = arr[j] ^ arr[j+1];
+					array[j]   = array[j] ^ array[j+1];
+					array[j+1] = array[j] ^ array[j+1];
+					array[j]   = array[j] ^ array[j+1];
 				}
 			}
 		}
@@ -36,23 +36,23 @@ public class BubbleSort
 	{
 
 		//array initialization and length
-		int arr[] = {8,0,1,50,22,4};
-		int length =  arr.length;
-		BubbleSort obj = new BubbleSort();
+		int array[] = {8,0,1,50,22,4};
+		int length =  array.length;
+		BubbleSort object = new BubbleSort();
 
 		//printing unsorted array
 		System.out.print("Before Sorting : ");
-		obj.printArray(arr,length);
+		object.printArray(array,length);
 
 		//sorting
-		obj.bubbleSort(arr, length);
+		object.bubbleSort(array, length);
 
 		//printing sorted array
 		System.out.print("After Sorting  : ");
-		obj.printArray(arr,length);
+		object.printArray(array,length);
 
 		//closing or removing object
-		obj = null;
+		object = null;
 		System.gc();
 
 	}
